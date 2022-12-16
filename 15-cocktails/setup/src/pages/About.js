e826f568f865e252e234c845aae5bb1fac822792
tincurar <tincurares13@gmail.com>
2022-12-16 16:29:12 +0100

    finished cocktail project

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Aliquid quis explicabo animi nemo nesciunt officia officiis, repellat sit blanditiis,
+        vitae laboriosam voluptatum. Aut laborum nemo praesentium, cum, mollitia, dolores
+        smodi aspernatur at perspiciatis asperiores ex a quis quaerat facilis incidunt.</p>
+    </section>
+  );
+};
 
-export default About
+export default About;
